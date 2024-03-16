eed0d405cfc7c3be10032e39b605fcf37d9c778c
Amir Ranjbr <mr.am.amir.ir@gmail.com>
2024-03-16 14:15:53 +0330

    build: add and refactor HTML CSS and JS

---
+++ src/script.js
@@ -0,0 +1,7 @@
+let sides = document.querySelectorAll(".panel-side");
+
+sides.forEach((side) => {
+  side.addEventListener("click", () => {
+    side.parentElement.classList.toggle("open");
+  });
+});
